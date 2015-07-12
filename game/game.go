53ba6351155f5cb786ca29cfa14e271de08867f3
Peter Hellberg <peter@c7.se>
2015-07-12 22:11:18 +0200

    Update game logic (added takeItem and dropItem)

--- game/game.go
+++ game/game.go
@@ -8,11 +8,13 @@ import (
 )
 
 const (
-	welcomeMessage       = "Adventure game!"
-	genericMessage       = "You don’t know how to do that."
-	notCarryingMessage   = "You are not carrying anything."
-	carryingMessage      = "You are carrying: "
-	youCanPerformMessage = "You can perform the following commands:\n"
+	welcomeMessage     = "Adventure game!"
+	byeMessage         = "Good bye! Player 1"
+	genericMessage     = "You don’t know how to do that."
+	notCarryingMessage = "You are not carrying anything."
+	carryingMessage    = "You are carrying: "
+	canPerformMessage  = "You can perform the following commands:\n"
+	cantGoMessage      = "You can’t go there!"
 )
 
 type Game struct {
@@ -36,21 +38,6 @@ func Start() {
 	g.Start()
 }
 
-func (g *Game) commands() map[string]func(string, []string) (string, error) {
-	return map[string]func(string, []string) (string, error){
-		"drop":      g.drop,
-		"exit":      g.exit,
-		"find":      g.generic,
-		"help":      g.help,
-		"inventory": g.inventory,
-		"kill":      g.generic,
-		"look":      g.look,
-		"take":      g.take,
-		"use":       g.use,
-		"walk":      g.walk,
-	}
-}
-
 func (g *Game) Setup() {
 	g.SetPrompt("▶ ")
 	g.RegisterGeneric(g.generic)
@@ -72,12 +59,25 @@ func (g *Game) Place() *Place {
 	return &Place{Name: "void", Paths: []string{"nowhere"}}
 }
 
+func (g *Game) commands() map[string]func(string, []string) (string, error) {
+	return map[string]func(string, []string) (string, error){
+		"drop":  g.drop,
+		"exit":  g.exit,
+		"help":  g.help,
+		"items": g.items,
+		"look":  g.look,
+		"take":  g.take,
+		"use":   g.use,
+		"walk":  g.walk,
+	}
+}
+
 func (g *Game) generic(cmd string, args []string) (string, error) {
 	return genericMessage, nil
 }
 
-func (g *Game) inventory(cmd string, args []string) (string, error) {
-	items := g.Player.Inventory
+func (g *Game) items(cmd string, args []string) (string, error) {
+	items := g.Player.Items
 
 	if len(items) == 0 {
 		return notCarryingMessage, nil
@@ -95,30 +95,22 @@ func (g *Game) help(cmd string, args []string) (string, error) {
 
 	sort.Strings(commandNames)
 
-	return youCanPerformMessage + strings.Join(commandNames, ", "), nil
+	return canPerformMessage + strings.Join(commandNames, ", "), nil
 }
 
 func (g *Game) look(cmd string, args []string) (string, error) {
 	p := g.Place()
 
-	l := []string{}
+	if p.Look != nil {
+		text, err := p.Look(g)
+		if err != nil {
+			return err.Error(), nil
+		}
 
-	switch p.VisitCount {
-	case 1:
-		l = append(l, "You are standing in the "+p.Name+" for the first time.")
-	default:
-		l = append(l, "You are standing in the "+p.Name)
+		return text + "\n\n" + p.describe(), nil
 	}
 
-	if len(p.Paths) > 0 {
-		l = append(l, "Paths: "+p.Paths.String())
-	}
-
-	if len(p.Items) > 0 {
-		l = append(l, "Items: "+p.Items.String())
-	}
-
-	return strings.Join(l, "\n"), nil
+	return p.describe(), nil
 }
 
 func (g *Game) take(cmd string, args []string) (string, error) {
@@ -129,13 +121,7 @@ func (g *Game) take(cmd string, args []string) (string, error) {
 	l := []string{}
 
 	for _, name := range args {
-		p := g.Place()
-
-		if item, ok := p.Items[name]; ok {
-			delete(p.Items, name)
-
-			g.Player.Inventory[name] = item
-
+		if item, ok := g.takeItem(name); ok {
 			if item.Take != nil {
 				return item.Take(g), nil
 			}
@@ -151,6 +137,19 @@ func (g *Game) take(cmd string, args []string) (string, error) {
 	return "You can’t take that which doesn’t exist.", nil
 }
 
+func (g *Game) takeItem(name string) (*Item, bool) {
+	p := g.Place()
+
+	if item, ok := p.Items[name]; ok {
+		delete(p.Items, name)
+		g.Player.AddItem(name, item)
+
+		return item, true
+	}
+
+	return nil, false
+}
+
 func (g *Game) drop(cmd string, args []string) (string, error) {
 	if len(args) == 0 {
 		return "You didn’t tell me what to drop.", nil
@@ -159,13 +158,7 @@ func (g *Game) drop(cmd string, args []string) (string, error) {
 	l := []string{}
 
 	for _, name := range args {
-		p := g.Place()
-
-		if item, ok := g.Player.Inventory[name]; ok {
-			delete(g.Player.Inventory, name)
-
-			p.Items[name] = item
-
+		if g.dropItem(name) {
 			l = append(l, "You dropped the "+name)
 		}
 	}
@@ -177,6 +170,17 @@ func (g *Game) drop(cmd string, args []string) (string, error) {
 	return "Unable to drop something you are not carrying.", nil
 }
 
+func (g *Game) dropItem(name string) bool {
+	if item, ok := g.Player.Items[name]; ok {
+		delete(g.Player.Items, name)
+		g.Place().AddItem(name, item)
+
+		return true
+	}
+
+	return false
+}
+
 func (g *Game) use(cmd string, args []string) (string, error) {
 	if len(args) == 0 {
 		return "You didn’t tell me what to use.", nil
@@ -184,7 +188,7 @@ func (g *Game) use(cmd string, args []string) (string, error) {
 
 	name := args[0]
 
-	if item, ok := g.Player.Inventory[name]; ok {
+	if item, ok := g.Player.Items[name]; ok {
 		if item.Use != nil {
 			return item.Use(g), nil
 		}
@@ -200,32 +204,48 @@ func (g *Game) walk(cmd string, args []string) (string, error) {
 		return "You need to specify where to go.", nil
 	}
 
-	target := strings.ToLower(args[0])
+	l := []string{}
 
-	if len(args) > 2 && args[0] == "to" && args[1] == "the" {
-		target = strings.ToLower(args[2])
-	} else if len(args) > 1 && args[0] == "to" {
-		target = strings.ToLower(args[1])
+	for _, name := range args {
+		p := g.Place()
+
+		target := strings.ToLower(name)
+
+		if p.Name == target {
+			return "You are already in the " + p.Name, nil
+		}
+
+		if p.IsNextTo(target) {
+			tp := g.Places[target]
+
+			var err error
+
+			if tp.Enter != nil {
+				_, err = tp.Enter(g)
+			}
+
+			if err != nil {
+				l = append(l, err.Error())
+			} else {
+				g.Player.Position = target
+				g.Places[target].VisitCount++
+
+				l = append(l, "You walked to the "+target)
+			}
+		} else {
+			l = append(l, "You are not next to the "+target+", staying in the "+g.Player.Position)
+		}
 	}
 
-	p := g.Place()
-
-	if p.Name == target {
-		return "You are already in the " + p.Name, nil
+	if len(l) > 0 {
+		return strings.Join(l, "\n"), nil
 	}
 
-	if p.IsNextTo(target) {
-		g.Player.Position = target
-		g.Places[target].VisitCount++
-
-		return "You walked to the " + target, nil
-	}
-
-	return "You can’t go there!", nil
+	return cantGoMessage, nil
 }
 
 func (g *Game) exit(cmd string, args []string) (string, error) {
 	g.Stop()
 
-	return "Good bye! Player 1", nil
+	return byeMessage, nil
 }
