3b4bdc3399c9bcb49d3745241cf3149d3b476865
Peter Hellberg <peter@c7.se>
2015-07-12 22:10:09 +0200

    Added Item and AddItem methods on player

--- game/player.go
+++ game/player.go
@@ -1,16 +1,41 @@
 package game
 
+import "fmt"
+
 type Player struct {
-	Name      string
-	Position  string
-	Inventory Items
+	Name     string
+	Position string
+	Items    Items
 }
 
 func NewPlayer() *Player {
 	return &Player{
 		Position: "kitchen",
-		Inventory: Items{
+		Items: Items{
 			"carrot": newCarrot(),
 		},
 	}
+}
+func (p *Player) Item(name string) (*Item, error) {
+	if item, ok := p.Items[name]; ok {
+		return item, nil
+	}
+
+	return &Item{}, fmt.Errorf("missing item")
+}
+
+func (p *Player) HasItem(name string) bool {
+	if _, ok := p.Items[name]; ok {
+		return true
+	}
+
+	return false
+}
+
+func (p *Player) AddItem(name string, item *Item) {
+	if p.Items == nil {
+		p.Items = Items{}
+	}
+
+	p.Items[name] = item
 }
